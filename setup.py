09d87abcbd706644377930d3d62da747d598149c
PhillipRambo <93056982+PhillipRambo@users.noreply.github.com>
2024-09-13 14:45:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='klayout_run',
     version='0.1',
     py_modules=['klayout_run', 'searchlogic'],
-    install_requires=['pick','os', 'subprocess'],  # Ensure dependencies are installed
+    install_requires=['pick'],  # Ensure dependencies are installed
     entry_points={
         'console_scripts': [
             'klayout_run=klayout_run:main'  # Entry point for the main script
